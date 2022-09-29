9e2ce1c7731c2182701bb88c3fe36d9af385cf94
MichaelRayven <themikerayven@gmail.com>
2022-09-29 22:51:32 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Piroll - design template
+
+Frontend build using a free design template: [Piroll](http://psd-html-css.ru/templates/piroll-besplatnyy-psd-shablon-dlya-portfolio)
+
+Stack:
+- Sass
+- Gulp
+- Typescript
+
+Technologies:
+- BEM
+- Adaptive
+- Flexbox
+- Grid
---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const {dest, series, src, watch} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const csso = require('gulp-csso');
+const include = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
+const ts = require('gulp-typescript');
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const clean = require('gulp-clean')
+const browsersync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+
+function html() {
+  return src('src/html/*.html')
+    .pipe(include({
+      indent: true
+    }))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(dest('dist'))
+}
+
+function scss() {
+  return src('src/css/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(csso())
+    .pipe(concat('index.css'))
+    .pipe(dest('dist'))
+}
+
+function typescript() {
+  return src('src/js/*.ts')
+    .pipe(ts({
+      noImplicitAny: true,
+      outFile: 'index.js'
+    }))
+    .pipe(uglify())
+    .pipe(dest('dist'))
+}
+
+function clear() {
+  return src('dist/*', {read: false, allowEmpty: true})
+    .pipe(clean({}))
+}
+
+function serve() {
+  browsersync.init({
+    server: {
+        baseDir: "./dist"
+    }
+  });
+
+  watch('src/assets/*', minimizeAssets).on('change', browsersync.reload)
+  watch('src/**/*.html', html).on('change', browsersync.reload)
+  watch('src/**/*.scss', scss).on('change', browsersync.reload)
+  watch('src/**/*.ts', typescript).on('change', browsersync.reload)
+}
+
+function minimizeAssets() {
+  return src('src/assets/*')
+    .pipe(imagemin())
+    .pipe(dest('dist/assets'))
+}
+
+exports.build = series(clear, html, scss, typescript, minimizeAssets)
+exports.serve = series(this.build, serve)
+exports.clean = clear
